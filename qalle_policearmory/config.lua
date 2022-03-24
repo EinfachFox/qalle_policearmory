652acea2a67e960e61c655b0841e3288e03ab2e0
EinfachFox <96894067+EinfachFox@users.noreply.github.com>
2022-03-24 11:57:21 +0100

    Add files via upload

---
+++ qalle_policearmory/config.lua
@@ -0,0 +1,23 @@
+Config = {}
+
+-- Turn this to false if you want everyone to use this.
+Config.OnlyPolicemen = true
+
+-- This is how much ammo you should get per weapon you take out.
+Config.ReceiveAmmo = 99999
+
+-- You don't need to edit these if you don't want to.
+Config.Armory = { ["x"] = 479.2490, ["y"] = -996.8333, ["z"] = 30.6919, ["h"] = 269.5743 }
+Config.ArmoryPed = { ["x"] = 480.3830, ["y"] = -996.6774, ["z"] = 30.6896, ["h"] = 83.6353, ["hash"] = "s_m_y_cop_01" }
+
+-- This is the available weapons you can pick out.
+Config.ArmoryWeapons = {
+	{ ["hash"] = "weapon_combatpistol", ["type"] = "pistol" },
+	{ ["hash"] = "weapon_stungun", ["type"] = "pistol" },
+	{ ["hash"] = "weapon_smg", ["type"] = "pistol" },
+	{ ["hash"] = "weapon_carbinerifle", ["type"] = "pistol" },
+	{ ["hash"] = "weapon_specialcarbine", ["type"] = "pistol" },
+	{ ["hash"] = "weapon_nightstick", ["type"] = "pistol" },
+	{ ["hash"] = "weapon_flashlight", ["type"] = "pistol" }
+}
+
